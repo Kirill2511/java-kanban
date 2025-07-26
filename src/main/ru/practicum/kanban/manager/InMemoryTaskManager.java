9bbc245bc9903243216c33a86b65f1c5c208018a
Кирилл Ларионов <k.larionov@isimplelab.com>
2025-07-27 00:50:04 +0500

    Refactor deleteAllSubtasks to remove subtasks from epics more efficiently

--- src/main/ru/practicum/kanban/manager/InMemoryTaskManager.java
+++ src/main/ru/practicum/kanban/manager/InMemoryTaskManager.java
@@ -209,7 +209,7 @@ public class InMemoryTaskManager implements TaskManager {
     public void deleteAllSubtasks() {
         subtasks.clear();
         for (Epic epic : epics.values()) {
-            epic.getSubtaskIds().clear();
+            epic.getSubtaskIds().forEach(subtasks::remove);
             updateEpicStatus(epic);
         }
     }
