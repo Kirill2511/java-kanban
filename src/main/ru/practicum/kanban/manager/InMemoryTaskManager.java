e70cc8106b32b24e9dd0f9f813c2395774acea86
Кирилл Ларионов <k.larionov@isimplelab.com>
2025-08-29 05:31:23 +0500

    Implement FileBackedTaskManager for persistent task storage and retrieval

--- src/main/ru/practicum/kanban/manager/InMemoryTaskManager.java
+++ src/main/ru/practicum/kanban/manager/InMemoryTaskManager.java
@@ -11,11 +11,11 @@ import java.util.List;
 import java.util.Map;
 
 public class InMemoryTaskManager implements TaskManager {
-    private final Map<Integer, Task> tasks = new HashMap<>();
-    private final Map<Integer, Epic> epics = new HashMap<>();
-    private final Map<Integer, Subtask> subtasks = new HashMap<>();
-    private final HistoryManager historyManager = Managers.getDefaultHistory();
-    private int nextId = 1;
+    protected final Map<Integer, Task> tasks = new HashMap<>();
+    protected final Map<Integer, Epic> epics = new HashMap<>();
+    protected final Map<Integer, Subtask> subtasks = new HashMap<>();
+    protected final HistoryManager historyManager = Managers.getDefaultHistory();
+    protected int nextId = 1;
 
     // Методы для обычных задач
     @Override
